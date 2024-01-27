282c83c07e38e764e552e118f0364855613a0dd5
Crowdbotics <team@crowdbotics.com>
2024-01-27 09:21:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dogfinder-45637",
+  url: "https://dogfinder-45637.botics.co",
+  api: "https://dogfinder-45637.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
